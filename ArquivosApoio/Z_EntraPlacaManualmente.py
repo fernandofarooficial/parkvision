e8e1b56c1cffcd75d8aaa28401348e1d695221ab
Fernando <fernando@exemplo.com>
2026-04-14 14:34:27 -0300

    [0002] Criação da tela operador

--- ArquivosApoio/Z_EntraPlacaManualmente.py
+++ ArquivosApoio/Z_EntraPlacaManualmente.py
@@ -1,35 +1,45 @@
-from db_lib_generico import gravar_movimento
+from visionlib.dblib import gravar_movimento
 from datetime import datetime
 
 movimentotest1 =  {'data': {'log_id': 99999, 'occurrence': 'Lpr',
                            'analytic_id': '929',
                            'region_dic': {'region0': [[1,1], [1,1], [1,1], [1,1], [1,1]]},
-                           'bbox_lst': None, 'video_stream': 'm', 'camera_id': 89,
-                           'camera_name': '6004 E', 'rtsp': 'rtsp:m',
-                           'address': 'BLAIA TESTE', 'description': None,
+                           'bbox_lst': None, 'video_stream': 'm', 'camera_id': 75,
+                           'camera_name': '6003 E', 'rtsp': 'rtsp:m',
+                           'address': 'Veraneio Entrada', 'description': None,
                            'coordinate': '{"latitude":"","longitude":""}',
-                           'created_at': '08/07/2025 15:25:39', 'rule': None,
+                           'created_at': '07/04/2026 15:00:00', 'rule': None,
                            'plate_value': 'USA1B99', 'car_color': 'white', 'car_color_confs': 0.91,
                            'image_base64': '/9j/4AAQSkZJRgABAQAAAQ'}}
 
 
-movimentotest2 =  {'data': {'log_id': 12345, 'occurrence': 'Lpr',
-                           'analytic_id': '124',
+movimentotest2 =  {'data': {'log_id': 88888, 'occurrence': 'Lpr',
+                           'analytic_id': '929',
                            'region_dic': {'region0': [[1,1], [1,1], [1,1], [1,1], [1,1]]},
-                           'bbox_lst': None, 'video_stream': 'm', 'camera_id': 199,
-                           'camera_name': '1234 TESTE APP VISIT ENTRADA', 'rtsp': 'rtsp:m',
-                           'address': 'UIRAPURUS', 'description': None,
+                           'bbox_lst': None, 'video_stream': 'm', 'camera_id': 75,
+                           'camera_name': '6003 E', 'rtsp': 'rtsp:m',
+                           'address': 'Veraneio Entrada', 'description': None,
                            'coordinate': '{"latitude":"","longitude":""}',
-                           'created_at': '30/10/2025 17:30:10', 'rule': None,
-                           'plate_value': 'JBH5ED0', 'car_color': 'white', 'car_color_confs': 0.91,
+                           'created_at': '07/04/2026 15:03:15', 'rule': None,
+                           'plate_value': 'FHZ1I06', 'car_color': 'white', 'car_color_confs': 0.91,
                            'image_base64': '/9j/4AAQSkZJRgABAQAAAQ'}}
 
-'''
+movimentotest3 =  {'data': {'log_id': 77777, 'occurrence': 'Lpr',
+                           'analytic_id': '929',
+                           'region_dic': {'region0': [[1,1], [1,1], [1,1], [1,1], [1,1]]},
+                           'bbox_lst': None, 'video_stream': 'm', 'camera_id': 75,
+                           'camera_name': '6003 E', 'rtsp': 'rtsp:m',
+                           'address': 'Veraneio Entrada', 'description': None,
+                           'coordinate': '{"latitude":"","longitude":""}',
+                           'created_at': '07/04/2026 15:05:30', 'rule': None,
+                           'plate_value': 'TEG9F15', 'car_color': 'white', 'car_color_confs': 0.91,
+                           'image_base64': '/9j/4AAQSkZJRgABAQAAAQ'}}
+
 dadosdic = gravar_movimento(movimentotest1)
-print(f"Placa: {dadosdic.get('placa','NA')} - IdCamera: {dadosdic.get('camera_id','NA')}")
-print(datetime.now())
-'''
+print(f"Processado placa sem cadastro: {dadosdic.get('placa','NA')} - IdCamera: {dadosdic.get('camera_id','NA')} - {datetime.now()}")
 
 dadosdic = gravar_movimento(movimentotest2)
-print(f"Placa: {dadosdic.get('placa','NA')} - IdCamera: {dadosdic.get('camera_id','NA')}")
-print(datetime.now())
+print(f"Processado sem vaga disponível: {dadosdic.get('placa','NA')} - IdCamera: {dadosdic.get('camera_id','NA')} - {datetime.now()}")
+
+dadosdic = gravar_movimento(movimentotest3)
+print(f"Processado sem permissão: {dadosdic.get('placa','NA')} - IdCamera: {dadosdic.get('camera_id','NA')} - {datetime.now()}")
